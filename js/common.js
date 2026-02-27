8af79bad9145dfd4c62cc9456c71ff81ea93b76c
Tran Vivian Le <giangngoctran.le@tuni.fi>
2026-02-27 23:35:58 +0200

    fix table display 24 hours only

--- js/common.js
+++ js/common.js
@@ -2,7 +2,7 @@ console.log("this is common.js")
 
 const BASE_URL = "https://api.open-meteo.com/v1/";
 
-const URL_TEMP = `${BASE_URL}/forecast?latitude=61.4991&longitude=23.7871&hourly=temperature_2m,weather_code&current=temperature_2m&timezone=auto`;
+const URL_TEMP = `${BASE_URL}/forecast?latitude=61.4991&longitude=23.7871&hourly=temperature_2m,weather_code&current=temperature_2m&timezone=auto&forecast_days=2`;
 
 const URL_CURRENT_WEATHER = `${BASE_URL}/forecast?latitude=61.4991&longitude=23.7871&current=weather_code&timezone=auto`;
 
